777a97cb36e7feab7ac9a18bba12d0b556741024
Jonathan Brandel <jono@with.in>
2018-04-26 20:54:46 -0700

    added context.resume

--- third-party/sound.js
+++ third-party/sound.js
@@ -159,6 +159,10 @@
         duration: this.buffer.duration - this._offset
       });
 
+      if (ctx && /suspended/.test(ctx.state)) {
+        ctx.resume();
+      }
+
       if (this.source) {
         this.stop();
       }
